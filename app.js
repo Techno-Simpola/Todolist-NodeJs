8d535d982cf7f1cac93ef9d974f3f93a9f8f48e4
Techno-Simpola <ashukumarsinghrathore123@gmail.com>
2021-07-24 05:47:26 +0530

    Updated App and did some tweaks

--- app.js
+++ app.js
@@ -141,8 +141,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-app.listen(port,function(){
-    console.log("The server is up and working at 3000");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
